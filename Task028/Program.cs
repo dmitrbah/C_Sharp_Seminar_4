0c4f3e9bd58ff6e5e8b38f14c8a6824919f30668
Dmitry <dmitrbah@mail.ru>
2023-10-09 16:53:42 +0300

    Added Task028

---
+++ Task028/Program.cs
@@ -0,0 +1,18 @@
+﻿int MultiplyNumbers(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+        mult = mult * i;
+        }
+    }
+    return mult;
+}
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = MultiplyNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
