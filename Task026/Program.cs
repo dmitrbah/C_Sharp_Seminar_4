f60030b509e9877e0f7d1ad8be1e5c2540b8a65b
Dmitry <dmitrbah@mail.ru>
2023-10-09 16:53:16 +0300

    Added Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,17 @@
+﻿int CountDigits(int num)
+{
+    if (num == 0) return 1;
+    int count = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        count = count + 1;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = CountDigits(number);
+Console.WriteLine($"Количество цифр в числе {number} равно {result}");
