ffb7eba32c4e03c81b8867853ca2a33e280163c9
Dmitry <dmitrbah@mail.ru>
2023-10-09 16:54:05 +0300

    Added Task030

---
+++ Task030/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.WriteLine("Ввведите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine()); 
+int[] arr = CreateArray(sizeArray);
+PrintArray(arr);
