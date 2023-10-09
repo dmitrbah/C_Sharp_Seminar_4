99a899013448d3a8ba901c1a8774044b7f12f079
Dmitry <dmitrbah@mail.ru>
2023-10-09 16:52:47 +0300

    Added Task024

---
+++ Task024/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+        sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
